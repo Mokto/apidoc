1a1c6f32ac10eaea546c9726573f46da986aeb51
Theo Mathieu <tmathieu.github@fastmail.com>
2024-05-11 13:17:12 +0200

    feat: secure the update route by api token

--- src/routes/api/openapi/+server.ts
+++ src/routes/api/openapi/+server.ts
@@ -1,10 +1,21 @@
 import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
 import { prepareDatabase } from '$lib/utils/db';
+import { error } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
 
-export const PUT: RequestHandler = async ({ request }) => {
+export const PUT: RequestHandler = async ({ request, url }) => {
+	if (!env.API_TOKEN) {
+		error(
+			403,
+			'API_TOKEN must be defined in the environment variables before using this endpoint.'
+		);
+	}
+	if (url.searchParams.get('apiToken') !== env.API_TOKEN) {
+		error(403, 'Invalid API token.');
+	}
+
 	const data = await request.json();
-
 	await prepareDatabase(data);
 
 	return json({});
